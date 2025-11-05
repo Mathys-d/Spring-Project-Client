fa8cfc64636696bb5a984fa1d362c8c02dcb0be0
mathys-d <mathys.deronne@le-campus-numerique.fr>
2025-11-05 15:00:19 +0100

    initial Commit

---
+++ src/main/java/org/example/springprojectclient/SpringProjectClientApplication.java
@@ -0,0 +1,13 @@
+package org.example.springprojectclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springprojectclient/SpringProjectClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springprojectclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
